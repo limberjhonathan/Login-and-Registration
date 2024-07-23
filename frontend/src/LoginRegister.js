37d7b12d4db1624414ee5b1b5d88ed6329b47a27
Limber Jhonathan <limberjhonathan@gmail.com>
2024-07-22 23:30:23 -0300

    Animação dos componentes

--- frontend/src/LoginRegister.js
+++ frontend/src/LoginRegister.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 export default function LoginRegister() {
+    function SwitchContent(){
+        const content = document.getElementById('content')
+        const registerBtn = document.getElementById('register')
+        const loginBtn = document.getElementById('login')
+
+        registerBtn.addEventListener('click', () => {
+            content.classList.add('active')
+        })
+        
+        loginBtn.addEventListener('click', () => {
+            content.classList.remove('active')
+        })
+
+    }
+
     return (
         <div className='content justify-content-center align-items-center d-flex shadow-lg' id='content'>
             {/* -----------------------Register------------------------ */}
@@ -10,13 +25,13 @@ export default function LoginRegister() {
                         <h1>Create Account</h1>
                     </div>
                     <div className='input-group mb-3'>
-                        <input type='text' placeholder='Name' className='form-control form-control-lg bg-lignt fs-6'></input>
+                        <input type='text' placeholder='Name' className='form-control form-control-lg bg-light fs-6' />
                     </div>
                     <div className='input-group mb-3'>
-                        <input type='email' placeholder='Email' className='form-control form-control-lg bg-lignt fs-6'></input>
+                        <input type='email' placeholder='Email' className='form-control form-control-lg bg-light fs-6' />
                     </div>
                     <div className='input-group mb-3'>
-                        <input type='password' placeholder='Password' className='form-control form-control-lg bg-lignt fs-6'></input>
+                        <input type='password' placeholder='Password' className='form-control form-control-lg bg-light fs-6' />
                     </div>
                     <div className='input-group mb-3 justify-content-center'>
                         <button className='btn border-white text-white w-50 fs-6'>Register</button>
@@ -30,14 +45,14 @@ export default function LoginRegister() {
                         <h1>Sign In</h1>
                     </div>
                     <div className='input-group mb-3'>
-                        <input type='email' placeholder='Email' className='form-control form-control-lg bg-light fs-6'></input>
+                        <input type='email' placeholder='Email' className='form-control form-control-lg bg-light fs-6' />
                     </div>
                     <div className='input-group mb-3'>
-                        <input type='password' placeholder='Password' className='form-control form-control-lg bg-light fs-6'></input>
+                        <input type='password' placeholder='Password' className='form-control form-control-lg bg-light fs-6' />
                     </div>
                     <div className='input-group mb-5 d-flex justify-content-between'>
                         <div className='form-check'>
-                            <input type='checkbox' className='form-check-input' id='formcheck'/>
+                            <input type='checkbox' className='form-check-input' id='formcheck' />
                             <label htmlFor='formcheck' className='form-check-label text-secondary'><small>Remember me</small></label>
                         </div>
                         <div className='forgot'>
@@ -48,6 +63,22 @@ export default function LoginRegister() {
                         <button className='btn border-white text-white w-50 fs-6'>Login</button>
                     </div>
                 </form>
+            </div>
+
+            {/* -------------------Switch panel-------------------------- */}
+            <div className='switch-content'>
+                <div className='switch'>
+                    <div className='switch-panel switch-left'>
+                        <h1>Hello, again</h1>
+                        <p>We are happy to see you back</p>
+                        <button className='hidden btn border-white text-white w-50 fs-6' id='login' onClick={SwitchContent}>Login</button>
+                    </div>
+                    <div className='switch-panel switch-right'>
+                        <h1>Welcome</h1>
+                        <p>Join Our Unique Platform, Explore a New Experience</p>
+                        <button className='hidden btn border-white text-white w-50 fs-6' id='register' onClick={SwitchContent}>Register</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
