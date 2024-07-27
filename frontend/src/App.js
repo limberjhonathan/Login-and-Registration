705c22abe27ec31134defdd140c6696610d73532
Limber Jhonathan <limberjhonathan@gmail.com>
2024-07-26 21:34:08 -0300

    ajustes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import LoginRegister from './LoginRegister'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginRegister from './LoginRegister';
 import Home from './Home';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LoginRegister/>}></Route>
-        <Route path='/register' element={<LoginRegister/>}></Route>
-        <Route path='/home' element={<Home />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<LoginRegister />} />
+                <Route path='/register' element={<LoginRegister />} />
+                <Route path='/home' element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
